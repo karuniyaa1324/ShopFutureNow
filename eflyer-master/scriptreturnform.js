1fc3fa46543cc48217a5235b59112c423413428f
karuniyaa1324 <skaruniyaa@gmail.com>
2024-08-13 20:31:48 +0530

    return form sorted

--- eflyer-master/scriptreturnform.js
+++ eflyer-master/scriptreturnform.js
@@ -1,8 +1,8 @@
 document.getElementById('returnform').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission for validation
 
-    const username = document.getElementById('ordernumber').value;
-    const password = document.getElementById('brandname').value;
+    const ordernumber = document.getElementById('ordernumber').value;
+    const brandname= document.getElementById('brandname').value;
     const errorMessage = document.getElementById('error-message');
 
     // Simple client-side validation
@@ -12,12 +12,12 @@ document.getElementById('returnform').addEventListener('submit', function(event)
     }
 
     // For demo purposes, a hardcoded username and password check
-    if (ordernumber == '123456' && brandname == 'Reequil Moisturiser') {
+    if (ordernumber == '123456' && brandname == 'reequil moisturiser') {
         window.location.href = 'locationtrack.html';
         // You would typically redirect to another page or perform further actions here
        
 
     } else {
-        window.location.href = 'locationtrack.html';
+        errorMessage.textContent= 'kindly check the details!';
     }
 });
